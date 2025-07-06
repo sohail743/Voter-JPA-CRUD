ce258dc3539017ebfaad0ecedda00d967178d230
sohail743 <64734474+sohail743@users.noreply.github.com>
2025-07-06 16:32:11 +0530

    Add files via upload

---
+++ voterproject/src/main/java/com/example/voterproject/VoterprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.voterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoterprojectApplication.class, args);
+	}
+
+}
---
+++ voterproject/src/test/java/com/example/voterproject/VoterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoterprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
