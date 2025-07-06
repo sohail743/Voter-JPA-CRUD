89eb3c55c4b8b29dc37aa8a675a5c48cac9d704f
sohail743 <sohailkhan5702@gmail.com>
2025-07-06 16:47:47 +0530

    My initial commit

--- voterproject/src/main/java/com/example/voterproject/VoterprojectApplication.java
+++ voterproject/src/main/java/com/example/voterproject/VoterprojectApplication.java
@@ -1,13 +1,25 @@
 package com.example.voterproject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VoterprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VoterprojectApplication.class, args);
+		
 	}
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello WOrld");
+		};
+	}
+	
+	
 
 }
